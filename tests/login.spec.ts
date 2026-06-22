76c7b8b59f33061dd426fb3935a3f12bcf0389eb
jairob92 <jairob92@gmail.com>
2026-06-22 17:40:44 -0500

    Dia 1 de 30 reto playwright

---
+++ tests/login.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+
+test('Login to HRM', async ({ page }) => {
+	
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+    await page.getByRole('textbox',{name:'Password'}).fill('admin123')
+    await page.getByRole('button',{name:'login'}).click()
+
+    await expect(page.getByRole('link',{name:'Admin'})).toBeVisible()
+});
+
+test('Login to HRM with wrong Credentials',async({page})=>{
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+    await page.getByRole('textbox',{name:'Password'}).fill('123456')
+    await page.getByRole('button',{name:'Login'}).click()
+
+    await expect(page.getByText('Invalid credentials')).toBeVisible()
+})
