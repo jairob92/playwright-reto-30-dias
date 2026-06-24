197cc10050e15d614e710a4dce1846eee743e19c
jairob92 <jairob92@gmail.com>
2026-06-24 11:28:37 -0500

    reto 4 de 30 playwright

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -31,3 +31,26 @@ test('check left menu options',async({page})=>{
     expect(currentMenuItems).toEqual(expectedItemsMenu)
     expect(currentMenuItems[0]).toEqual('Admin')
 })
+
+test('Navigate throught left panel',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox',{name:'Username'}).fill('Admin')
+    await page.getByRole('textbox',{name:'Password'}).fill('admin123')
+    await page.getByRole('button',{name:'login'}).click()
+
+    const leftMenuItem=page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount= await leftMenuItem.count()
+    console.log('Current menu items count', currentMenuItemsCount)
+
+    for(let i=0; i<currentMenuItemsCount; i++){
+        
+        const menuItem=  leftMenuItem.nth(i)
+        const menuText= await menuItem.innerText()
+        await menuItem.click({timeout:5000})
+        if(menuText=='Maintenance'){
+            await expect(page).toHaveURL(/maintenance\/purgeEmployee/)
+            await page.goBack()
+            await expect(page.getByLabel('Sidepanel')).toBeVisible();
+        }
+    }
+})
